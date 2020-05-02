1281ade08fcdc6a81293bebbde61e77d6e60c3d7
Dev Boldly <devboldly@gmail.com>
2020-05-02 14:24:55 -0500

    Build, version, and publish.

--- dist/components/UncachedImage.d.ts
+++ dist/components/UncachedImage.d.ts
@@ -1,6 +1,11 @@
 import * as React from 'react';
-interface UncachedImageProps {
+export interface UncachedImageProps {
+    /** Optional. You can provide your own cache buster, which will be URI encoded and added as a query parameter. If you make this constant, cache-busting will effectively be disabled. When not specified, one is generated automatically. */
     cacheBuster?: React.ReactText;
 }
-export declare function UncachedImage(props: UncachedImageProps & React.ImgHTMLAttributes<HTMLImageElement>): JSX.Element;
-export {};
+/**
+ * See documentation: https://devboldly.github.io/react-uncached-image/
+ *
+ * An UncachedImage adds a cache-busting query param to your image `src` to ensure the browser requests the image from the server on every render.
+ */
+export declare function UncachedImage({ cacheBuster, ...imgProps }: UncachedImageProps & React.ImgHTMLAttributes<HTMLImageElement>): JSX.Element;
--- dist/components/UncachedImage.js
+++ dist/components/UncachedImage.js
@@ -10,6 +10,17 @@ var __assign = (this && this.__assign) || function () {
     };
     return __assign.apply(this, arguments);
 };
+var __rest = (this && this.__rest) || function (s, e) {
+    var t = {};
+    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
+        t[p] = s[p];
+    if (s != null && typeof Object.getOwnPropertySymbols === "function")
+        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
+            if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i]))
+                t[p[i]] = s[p[i]];
+        }
+    return t;
+};
 var __importStar = (this && this.__importStar) || function (mod) {
     if (mod && mod.__esModule) return mod;
     var result = {};
@@ -19,16 +30,33 @@ var __importStar = (this && this.__importStar) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var React = __importStar(require("react"));
-function UncachedImage(props) {
-    var src = props.src;
-    var cacheBuster = props.cacheBuster ? props.cacheBuster : new Date().getTime() + Math.round(Math.random() * 999999);
+/**
+ * See documentation: https://devboldly.github.io/react-uncached-image/
+ *
+ * An UncachedImage adds a cache-busting query param to your image `src` to ensure the browser requests the image from the server on every render.
+ */
+function UncachedImage(_a) {
+    var cacheBuster = _a.cacheBuster, imgProps = __rest(_a, ["cacheBuster"]);
+    var src = imgProps.src;
+    cacheBuster = cacheBuster ? cacheBuster : randChars(10);
     if (typeof src === 'string') {
         var paramSymbol = src.indexOf('?') >= 0 ? '&' : '?';
         src = "" + src + paramSymbol + encodeURIComponent(cacheBuster);
     }
-    var imgProps = __assign(__assign({}, props), { src: src });
-    // Remove our own props
-    delete imgProps.cacheBuster;
-    return React.createElement("img", __assign({}, imgProps));
+    return React.createElement("img", __assign({}, imgProps, { src: src, alt: imgProps.alt }));
 }
 exports.UncachedImage = UncachedImage;
+/**
+ * Generate a string of random alphanumeric characters, of the length provided.
+ *
+ * @param length The length of the string.
+ */
+var randChars = function (length) {
+    var result = '';
+    var characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for (var i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+};
