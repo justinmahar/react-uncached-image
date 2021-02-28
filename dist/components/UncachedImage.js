5a14a9224261f321b67b836f9a443f05385faf49
Justin Mahar <contact@justinmahar.com>
2021-02-28 17:16:06 -0600

    Build, version, and publish.

--- dist/components/UncachedImage.js
+++ dist/components/UncachedImage.js
@@ -1,48 +1,47 @@
 "use strict";
-var __assign =
-  (this && this.__assign) ||
-  function() {
-    __assign =
-      Object.assign ||
-      function(t) {
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
         for (var s, i = 1, n = arguments.length; i < n; i++) {
-          s = arguments[i];
-          for (var p in s)
-            if (Object.prototype.hasOwnProperty.call(s, p)) t[p] = s[p];
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
         }
         return t;
-      };
+    };
     return __assign.apply(this, arguments);
-  };
-var __rest =
-  (this && this.__rest) ||
-  function(s, e) {
-    var t = {};
-    for (var p in s)
-      if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
-        t[p] = s[p];
-    if (s != null && typeof Object.getOwnPropertySymbols === "function")
-      for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
-        if (
-          e.indexOf(p[i]) < 0 &&
-          Object.prototype.propertyIsEnumerable.call(s, p[i])
-        )
-          t[p[i]] = s[p[i]];
-      }
-    return t;
-  };
-var __importStar =
-  (this && this.__importStar) ||
-  function(mod) {
+};
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
     if (mod && mod.__esModule) return mod;
     var result = {};
-    if (mod != null)
-      for (var k in mod)
-        if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
-    result["default"] = mod;
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
     return result;
-  };
+};
+var __rest = (this && this.__rest) || function (s, e) {
+    var t = {};
+    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
+        t[p] = s[p];
+    if (s != null && typeof Object.getOwnPropertySymbols === "function")
+        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
+            if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i]))
+                t[p[i]] = s[p[i]];
+        }
+    return t;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.UncachedImage = void 0;
 var React = __importStar(require("react"));
 /**
  * See documentation: [UncachedImage](https://justinmahar.github.io/react-uncached-image/UncachedImage)
@@ -50,18 +49,14 @@ var React = __importStar(require("react"));
  * An UncachedImage adds a cache-busting query param to your image `src` to ensure the browser requests the image from the server on every render.
  */
 function UncachedImage(_a) {
-  var cacheBuster = _a.cacheBuster,
-    imgProps = __rest(_a, ["cacheBuster"]);
-  var src = imgProps.src;
-  cacheBuster = cacheBuster ? cacheBuster : randChars(10);
-  if (typeof src === "string") {
-    var paramSymbol = src.indexOf("?") >= 0 ? "&" : "?";
-    src = "" + src + paramSymbol + encodeURIComponent(cacheBuster);
-  }
-  return React.createElement(
-    "img",
-    __assign({}, imgProps, { src: src, alt: imgProps.alt })
-  );
+    var cacheBuster = _a.cacheBuster, imgProps = __rest(_a, ["cacheBuster"]);
+    var src = imgProps.src;
+    cacheBuster = cacheBuster ? cacheBuster : randChars(10);
+    if (typeof src === 'string') {
+        var paramSymbol = src.indexOf('?') >= 0 ? '&' : '?';
+        src = "" + src + paramSymbol + encodeURIComponent(cacheBuster);
+    }
+    return React.createElement("img", __assign({}, imgProps, { src: src, alt: imgProps.alt }));
 }
 exports.UncachedImage = UncachedImage;
 /**
@@ -69,12 +64,12 @@ exports.UncachedImage = UncachedImage;
  *
  * @param length The length of the string.
  */
-var randChars = function(length) {
-  var result = "";
-  var characters = "abcdefghijklmnopqrstuvwxyz0123456789";
-  var charactersLength = characters.length;
-  for (var i = 0; i < length; i++) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength));
-  }
-  return result;
+var randChars = function (length) {
+    var result = '';
+    var characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for (var i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
 };
