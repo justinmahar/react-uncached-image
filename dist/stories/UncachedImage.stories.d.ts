bd43672267e3b3d42597bfefd8f539e0b7ad4d75
Justin Mahar <contact@justinmahar.com>
2023-01-11 23:02:35 -0500

    Build, version, and publish.

---
+++ dist/stories/UncachedImage.stories.d.ts
@@ -0,0 +1,11 @@
+/// <reference types="react" />
+import { ComponentMeta } from '@storybook/react';
+import { UncachedImage } from '../components/UncachedImage';
+declare const _default: ComponentMeta<typeof UncachedImage>;
+export default _default;
+export declare const CachedVsUncachedImages: {
+    (): JSX.Element;
+    story: {
+        name: string;
+    };
+};
